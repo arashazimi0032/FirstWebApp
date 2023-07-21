5247e4aec102d655de522f32d95829ecb7ec49e5
arashazimi0032 <arashazimi0032@gmail.com>
2023-07-21 23:42:03 +0330

    Create Register and Login Pages

---
+++ FirstWebApp/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using FirstWebApp.Data;
+using FirstWebApp.Models;
+using FirstWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            LoginViewModel response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                bool passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                TempData["Error"] = "Wrong credintial. Please try again.";
+                return View(loginViewModel);
+            }
+            TempData["Error"] = "Wrong credintial. Please try again.";
+            return View(loginViewModel);
+        }
+
+        public IActionResult Register()
+        {
+            RegisterViewModel response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already exist.";
+                return View(registerViewModel);
+            }
+
+            AppUser newAppUser = new AppUser
+            {
+                Email = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newAppUser);
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newAppUser, UserRoles.User);
+            }
+            return RedirectToAction("Index", "Race");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Race");
+        }
+    }
+}
---
+++ FirstWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email Address is required.")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
